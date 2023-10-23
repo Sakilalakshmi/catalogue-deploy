58f651e9d9896b68b2b5088bad9fa22f8e971e7d
Sakilalakshmi <sakilalakshmi95@gmail.com>
2023-10-23 15:41:19 +0530

    app version

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -4,4 +4,4 @@ echo "app version: $APP_VERSION"
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
 cd /tmp
-ansible-pull -U https://github.com/Chinna9967/ansible-roles-sa.git -e component=catalogue -e app_version=$APP_VERSION main.yaml
+ansible-pull -U https://github.com/Sakilalakshmi/ansible-roles-sa.git -e component=catalogue -e app_version=$APP_VERSION main.yaml
