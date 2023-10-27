0642a08ca94f2c645edf2d8a59c5d25d8e81fecf
Sakilalakshmi <sakilalakshmi95@gmail.com>
2023-10-27 22:17:04 +0530

    version changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,35 +14,35 @@ module "catalogue_instance" {
   )
 }
 
-# resource "null_resource" "cluster" {
-#   # Changes to any instance of the cluster requires re-provisioning
-#   triggers = {
-#     instance_id = module.catalogue_instance.id
-#   }
+resource "null_resource" "cluster" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.catalogue_instance.id
+  }
 
-#   # Bootstrap script can run on any instance of the cluster
-#   # So we just choose the first in this case
-#   connection {
-#     type     = "ssh"
-#     user     = "centos"
-#     password = "DevOps321"
-#     host     = module.catalogue_instance.private_ip
-#   }
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = module.catalogue_instance.private_ip
+  }
 
-#   #copy the file
-#   provisioner "file" {
-#     source      = "catalogue.sh"
-#     destination = "/tmp/catalogue.sh"
-#   }
+  #copy the file
+  provisioner "file" {
+    source      = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
 
-#   provisioner "remote-exec" {
-#     # Bootstrap script called with private_ip of each node in the cluster
-#     inline = [
-#       "chmod +x /tmp/catalogue.sh",
-#       "sudo sh /tmp/catalogue.sh ${var.app_version}"
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/catalogue.sh",
+      "sudo sh /tmp/catalogue.sh ${var.app_version}"
+    ]
+  }
+}
 
 # ## stop the instance and take AMI.
 # resource "aws_ec2_instance_state" "catalogue_instance" {
