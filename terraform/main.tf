90854f521c7b40b6c5266aa65afb482a069cc67c
Sakilalakshmi <sakilalakshmi95@gmail.com>
2023-10-29 18:34:46 +0530

    version added

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "catalogue_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   ami = data.aws_ami.devops_ami.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [data.aws_ssm_parameter.mongodb_sg_id.value]
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
   # it should be in Roboshop DB subnet
   subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
   //user_data = file("catalogue.sh")
